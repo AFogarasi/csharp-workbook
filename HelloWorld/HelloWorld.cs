e196008ba277423421560fc9ddd335cbd217abe8
Andy Fogarasi <andy.fogarasi@gmail.com>
2019-04-04 18:39:32 -0500

    first week upload

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
+//Hello World Code
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
-        }
+        
+//Write a C# program that takes two numbers as input, adds them together, and displays the result of that operation
+
+            int number1 = 0;
+            int number2 = 0;
+
+            Console.WriteLine("Please a number greater than 0: ");
+            number1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Please enter another number greater than 0: ");
+            number2 = int.Parse(Console.ReadLine());
+       
+        }   
     }
 }
+
+
